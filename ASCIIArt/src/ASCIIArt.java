2804394931c604c201797e0013b85eb346030bb7
s-shenc <s-shenc@53S500036561.it.bsd405.org>
2018-09-19 08:50:38 -0700

    Turning in project!

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -14,4 +14,160 @@
  *
  */
 
+public class ASCIIArt {
+	public static void zero() {
+		System.out.println("OOOOOOOOooooOOOOOOOO\r\n" + 
+						   "OOOOOOOo0000oOOOOOOO\r\n" + 
+						   "OOOOOo00oooo00oOOOOO\r\n" + 
+						   "OOOOo00oOOo0000oOOOO\r\n" + 
+						   "OOOOo00oOo00o00oOOOO\r\n" + 
+						   "OOOOo00oo00oo00oOOOO\r\n" + 
+						   "OOOOo00oo00oo00oOOOO\r\n" + 
+						   "OOOOo00o00oOo00oOOOO\r\n" + 
+						   "OOOOo0000oOOo00oOOOO\r\n" + 
+						   "OOOOOo00oooo00oOOOOO\r\n" + 
+						   "OOOOOOOo0000oOOOOOOO\r\n" + 
+						   "OOOOOOOOooooOOOOOOOO");
+	}
+	public static void one() {
+		System.out.println("|||||||''''|||||||||\r\n" + 
+						   "||||||'1111'||||||||\r\n" + 
+						   "|||||'11'11'||||||||\r\n" + 
+						   "||||'11''11'||||||||\r\n" + 
+						   "|||||''|'11'||||||||\r\n" + 
+						   "||||||||'11'||||||||\r\n" + 
+						   "||||||||'11'||||||||\r\n" + 
+						   "||||||||'11'||||||||\r\n" + 
+						   "||||||||'11'||||||||\r\n" + 
+						   "||||||'''11'''||||||\r\n" + 
+						   "|||||'11111111'|||||\r\n" + 
+						   "||||||''''''''||||||");
+	}
+	public static void two() {
+		System.out.println("ZZZZZZZ%%%%%%ZZZZZZZ\r\n" + 
+						   "ZZZZZ%%222222%%ZZZZZ\r\n" + 
+						   "ZZZZ%222%%%%222%ZZZZ\r\n" + 
+						   "ZZZ%22%%ZZZZZ%22%ZZZ\r\n" + 
+						   "ZZZZ%%ZZZZZZZZ%22%ZZ\r\n" + 
+						   "ZZZZZZZZZZZZZ%%22%ZZ\r\n" + 
+						   "ZZZZZZZZZZZ%%222%ZZZ\r\n" + 
+						   "ZZZZZZZZ%%%222%%ZZZZ\r\n" + 
+						   "ZZZZZZ%%222%%%ZZZZZZ\r\n" + 
+						   "ZZZZ%%222%%%%%%%%ZZZ\r\n" + 
+						   "ZZZ%2222222222222%ZZ\r\n" + 
+						   "ZZZZ%%%%%%%%%%%%%ZZZ");
+	}
+	public static void three() {
+		System.out.println("EEEEEEE[[[[[[EEEEEEE\r\n" + 
+						   "EEEEE[[333333[[EEEEE\r\n" + 
+						   "EEEE[333[[[[333[EEEE\r\n" + 
+						   "EEE[33[[EEEE[[33[EEE\r\n" + 
+						   "EEEE[[EEE[[[[[33[EEE\r\n" + 
+						   "EEEEEEEE[333333[EEEE\r\n" + 
+						   "EEEEEEEEE[[[[[33[EEE\r\n" + 
+						   "EEE[[EEEEEEEEE[33[EE\r\n" + 
+						   "EE[33[[EEEEEE[[33[EE\r\n" + 
+						   "EEE[333[[[[[[333[EEE\r\n" + 
+						   "EEEE[[33333333[[EEEE\r\n" + 
+						   "EEEEEE[[[[[[[[EEEEEE");
+	}
+	public static void four() {
+		System.out.println("+++++++++^^^^^++++++\r\n" + 
+						   "++++++++^44444^+++++\r\n" + 
+						   "+++++++^44^^44^+++++\r\n" + 
+						   "++++++^44^+^44^+++++\r\n" + 
+						   "+++++^44^++^44^+++++\r\n" + 
+						   "++++^44^+++^44^+++++\r\n" + 
+						   "+++^44^++++^44^+++++\r\n" + 
+						   "++^44^^^^^^^44^^^^++\r\n" + 
+						   "++^444444444444444^+\r\n" + 
+						   "+++^^^^^^^^^44^^^^++\r\n" + 
+						   "+++++++++++^44^+++++\r\n" + 
+						   "++++++++++++^^++++++");
+	}
+	public static void five() {
+		System.out.println("SSSS$$$$$$$$$$$$SSSS\r\n" + 
+						   "SSS$555555555555$SSS\r\n" + 
+						   "SSS$55$$$$$$$$$$SSSS\r\n" + 
+						   "SSS$55$$$$$$$SSSSSSS\r\n" + 
+						   "SSS$555555555$$$SSSS\r\n" + 
+						   "SSSS$$$$$$$$5555$SSS\r\n" + 
+						   "SSSSSSSSSSSS$$$55$SS\r\n" + 
+						   "SSSSSSSSSSSSSSS$55$S\r\n" + 
+						   "SSSSSSSSSSSS$$$55$SS\r\n" + 
+						   "SSSS$$$$$$$$5555$SSS\r\n" + 
+						   "SSS$555555555$$$SSSS\r\n" + 
+						   "SSSS$$$$$$$$$SSSSSSS");
+	}
+	public static void six() {
+		System.out.println("bbbbbbllllllllbbbbbb\r\n" + 
+						   "bbbbll66666666llbbbb\r\n" + 
+						   "bbbl666lllll6666lbbb\r\n" + 
+						   "bbl666lbbbbbllllbbbb\r\n" + 
+						   "bbl66lllllllllbbbbbb\r\n" + 
+						   "bbl66l66666666lbbbbb\r\n" + 
+						   "bbl6666llllll666lbbb\r\n" + 
+						   "bbl666lbbbbbbll66lbb\r\n" + 
+						   "bbl666lbbbbbbll66lbb\r\n" + 
+						   "bbbl666llllll666lbbb\r\n" + 
+						   "bbbbll66666666llbbbb\r\n" + 
+						   "bbbbbbllllllllbbbbbb");
+	}
+	public static void seven() {
+		System.out.println("LLLLiiiiiiiiiiiiLLLL\r\n" + 
+						   "LLLi777777777777iLLL\r\n" + 
+						   "LLLLiiiiiiiiii77iLLL\r\n" + 
+						   "LLLLLLLLLLLLi77iLLLL\r\n" + 
+						   "LLLLLLLLLLLi77iLLLLL\r\n" + 
+						   "LLLLLLLLLLi77iLLLLLL\r\n" + 
+						   "LLLLLLLLLi77iLLLLLLL\r\n" + 
+						   "LLLLLLLLi77iLLLLLLLL\r\n" + 
+						   "LLLLLLLi77iLLLLLLLLL\r\n" + 
+						   "LLLLLLi77iLLLLLLLLLL\r\n" + 
+						   "LLLLLi77iLLLLLLLLLLL\r\n" + 
+						   "LLLLLLiiLLLLLLLLLLLL");
+	}
+	public static void eight() {
+		System.out.println("0000000oooooo0000000\r\n" + 
+						   "00000oo888888oo00000\r\n" + 
+						   "0000o888oooo888o0000\r\n" + 
+						   "000o88oo0000oo88o000\r\n" + 
+						   "000o888oooooo888o000\r\n" + 
+						   "0000oo88888888oo0000\r\n" + 
+						   "000o888oooooo888o000\r\n" + 
+						   "00o88oo000000oo88o00\r\n" + 
+						   "00o88oo000000oo88o00\r\n" + 
+						   "000o888oooooo888o000\r\n" + 
+						   "0000oo88888888oo0000\r\n" + 
+						   "000000oooooooo000000");
+	}
+	public static void nine() {
+		System.out.println("ppppppp-------pppppp\r\n" + 
+						   "ppppp--9999999--pppp\r\n" + 
+						   "pppp-999-----999-ppp\r\n" + 
+						   "ppp-99--ppppp--99-pp\r\n" + 
+						   "ppp-99--ppppp-999-pp\r\n" + 
+						   "pppp-999-----9999-pp\r\n" + 
+						   "ppppp-99999999-99-pp\r\n" + 
+						   "pppppp---------99-pp\r\n" + 
+						   "ppppppppppppp-999-pp\r\n" + 
+						   "ppppp--------999-ppp\r\n" + 
+						   "pppp-999999999--pppp\r\n" + 
+						   "ppppp---------pppppp");
+	}
+	public static void main(String[] args) {
+		zero();
+		one();
+		two();
+		three();
+		four();
+		five();
+		six();
+		seven();
+		eight();
+		nine();
+		
+	}
+}
+
 // YOUR CODE GOES HERE :)
